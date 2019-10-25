a47d36d025997fa99314a0ff026e5b7260af3097
jasper debaene <jasperdebaene@hotmail.be>
2019-10-25 15:26:04 +0200

    initial commit, A and B buttons colorize

--- js/pixelate.js
+++ js/pixelate.js
@@ -8,14 +8,24 @@ document.addEventListener('DOMContentLoaded', (event) => {
         controller.connect()
         .then(() => {
             console.log("Connected to BLE controller");
-            cursor.change_color({r:0, g:0, b:200});
             controller.addEventListener('touch', (event) => {
                 switch(event.touchedKey) {
                     case 'R': cursor.move_right(); break;
                     case 'L': cursor.move_left(); break;
                     case 'U': cursor.move_up(); break;
                     case 'D': cursor.move_down(); break;
-                    case 'X': cursor.colorize({r: 200, g:0, b:0}); break;
+                    case 'A': 
+                    cursor.change_color({r:200, g:0, b:0});
+                    cursor.colorize();
+                    break;
+                    case 'B':
+                    cursor.change_color({r:0, g:200, b:0});
+                    cursor.colorize();
+                    break;
+                    case 'X': 
+                    cursor.change_color({r:0, g:0, b:200});
+                    cursor.colorize();
+                    break;
                 }
             });
         }).catch((error) => {
